c7e4fcb64cc0b43fcd91edbe7a0fc730370a9ef1
cuneyt <cuneytky@gmail.com>
2024-01-12 12:22:12 +0300

    EUAC2-9 added

---
+++ src/main/java/com/cydeo/entity/Product.java
@@ -0,0 +1,31 @@
+package com.cydeo.entity;
+
+import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.ProductUnit;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product extends BaseEntity {
+
+    private String name;
+    private int quantityInStock;
+    private int lowLimitAlert;
+
+    @Enumerated(EnumType.STRING)
+    private ProductUnit productUnit;
+
+    @ManyToOne
+    @JoinColumn(name = "category")
+    Category category;
+}
