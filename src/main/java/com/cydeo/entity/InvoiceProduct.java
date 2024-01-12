647490722a35a7ad961364f778e06d3d47e616a3
cuneyt <cuneytky@gmail.com>
2024-01-12 12:45:13 +0300

    EUAC2-11 added

---
+++ src/main/java/com/cydeo/entity/InvoiceProduct.java
@@ -0,0 +1,34 @@
+package com.cydeo.entity;
+
+import com.cydeo.entity.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "invoice_products")
+public class InvoiceProduct extends BaseEntity {
+
+    private int quantity;
+    private BigDecimal price;
+    private int tax;
+    private BigDecimal profitLoss;
+    private int remainingQuantity;
+
+    @ManyToOne
+    @JoinColumn(name = "invoice_id")
+    private Invoice invoice;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
