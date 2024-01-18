bd991e0b46ed1420bb24e94137c39a50011496f0
cuneyt <cuneytky@gmail.com>
2024-01-18 13:24:36 +0300

    EUAC2-17: added ClientVendorController, ClientVendorService, ClientVendorServiceImpl and converter methods

--- src/main/java/com/cydeo/repository/ClientVendorRepository.java
+++ src/main/java/com/cydeo/repository/ClientVendorRepository.java
@@ -1,4 +1,10 @@
 package com.cydeo.repository;
 
-public interface ClientVendorRepository {
+import com.cydeo.entity.ClientVendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientVendorRepository extends JpaRepository <ClientVendor,Long>{
+    ClientVendor findByClientVendorNameAndIsDeleted(String username, Boolean deleted);
 }
