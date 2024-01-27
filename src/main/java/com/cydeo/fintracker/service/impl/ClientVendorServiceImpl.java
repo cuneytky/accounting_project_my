aa35ed465eae74c442345b15bc5bdcae518d36ef
cuneyt <cuneytky@gmail.com>
2024-01-27 19:06:32 +0300

    clientVendor changed package

--- src/main/java/com/cydeo/fintracker/service/impl/ClientVendorServiceImpl.java
+++ src/main/java/com/cydeo/fintracker/service/impl/ClientVendorServiceImpl.java
@@ -3,6 +3,7 @@ package com.cydeo.fintracker.service.impl;
 
 import com.cydeo.fintracker.dto.ClientVendorDto;
 import com.cydeo.fintracker.dto.CompanyDto;
+import com.cydeo.fintracker.dto.CountryDto;
 import com.cydeo.fintracker.dto.UserDto;
 import com.cydeo.fintracker.entity.ClientVendor;
 import com.cydeo.fintracker.entity.Company;
@@ -15,14 +16,12 @@ import com.cydeo.fintracker.service.SecurityService;
 import com.cydeo.fintracker.util.MapperUtil;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-
-//    @Value("${COUNTRIES_API_KEY}")
-//    private String API_COUNTRIES_KEY;
 @Service
 public class ClientVendorServiceImpl implements ClientVendorService {
 
@@ -44,35 +43,37 @@ public class ClientVendorServiceImpl implements ClientVendorService {
 
         Optional<List<ClientVendor>> storedClientVendors = clientVendorRepository.findByClientVendorType(clientVendorType);
 
-        if(storedClientVendors.isEmpty()){
+        if (storedClientVendors.isEmpty()) {
             throw new NoSuchElementException();
 
         }
         List<ClientVendor> clientVendors = storedClientVendors.get();
         return clientVendors.stream()
-                .map(each -> mapperUtil.convert(each,new ClientVendorDto()))
+                .map(each -> mapperUtil.convert(each, new ClientVendorDto()))
                 .collect(Collectors.toList());
 
     }
 
     @Override
     public List<ClientVendorDto> getAll() {
+        Long loggedUserCompanyId = securityService.getLoggedInUser().getCompany().getId();
 
-        Optional<List<ClientVendor>> clientVendorlist = clientVendorRepository.findAllByIsDeleted(false);
+        List<ClientVendor> clientVendorListByCompanyId = clientVendorRepository.findAllByCompany_IdAndIsDeleted(loggedUserCompanyId, false);
 
-        if(clientVendorlist.isEmpty()){
+        List<ClientVendor> clientVendorlist = clientVendorRepository.findAllByCompany_IdAndIsDeleted(loggedUserCompanyId, false);
+
+        if (clientVendorlist.isEmpty()) {
             throw new ClientVendorNotFoundException("There are no ClientVendor found");
         }
 
-        List<ClientVendor> storedClientVendorList = clientVendorlist.get();
-        return storedClientVendorList.stream().map(clientVendor ->
-                mapperUtil.convert(clientVendor,new ClientVendorDto())).collect(Collectors.toList());
+        return clientVendorlist.stream().sorted(Comparator.comparing(ClientVendor::getClientVendorType).reversed().thenComparing(ClientVendor::getClientVendorName)).map(clientVendor ->
+                mapperUtil.convert(clientVendor, new ClientVendorDto())).collect(Collectors.toList());
     }
 
     @Override
     public List<ClientVendorDto> getAllClientVendorsCompany() {
         UserDto loggedInUser = securityService.getLoggedInUser();
-        List<ClientVendor> clientVendors = clientVendorRepository.findAllByCompanyId(loggedInUser.getCompany().getId());
+        List<ClientVendor> clientVendors = clientVendorRepository.findByCompanyIdAndIsDeleted(loggedInUser.getCompany().getId(),false);
         return clientVendors.stream().map(clientVendor -> {
                     boolean hasInvoice = isClientHasInvoice(clientVendor.getId());
                     ClientVendorDto convert = mapperUtil.convert(clientVendor, new ClientVendorDto());
@@ -89,16 +90,16 @@ public class ClientVendorServiceImpl implements ClientVendorService {
     @Override
     public ClientVendorDto findById(Long id) {
 
-        ClientVendor clientVendor = clientVendorRepository.findById(id).orElseThrow(()->
-                new NoSuchElementException("Client vendor cannot be found: "+id));
-        return mapperUtil.convert(clientVendor,new ClientVendorDto());
+        ClientVendor clientVendor = clientVendorRepository.findById(id).orElseThrow(() ->
+                new NoSuchElementException("Client vendor cannot be found: " + id));
+        return mapperUtil.convert(clientVendor, new ClientVendorDto());
     }
 
     @Override
     public ClientVendorDto findByClientVendorName(String username) {
 
-        ClientVendor clientVendor= clientVendorRepository.findByClientVendorNameAndIsDeleted(username,false);
-        return mapperUtil.convert(clientVendor,new ClientVendorDto());
+        ClientVendor clientVendor = clientVendorRepository.findByClientVendorNameAndIsDeleted(username, false);
+        return mapperUtil.convert(clientVendor, new ClientVendorDto());
     }
 
     @Override
@@ -126,7 +127,7 @@ public class ClientVendorServiceImpl implements ClientVendorService {
     }
 
     @Override
-    public ClientVendorDto update(Long id,ClientVendorDto clientVendor) {
+    public ClientVendorDto update(Long id, ClientVendorDto clientVendor) {
 
         //Find current ClientVendor
         ClientVendor clientVendor1 = clientVendorRepository.findById(id).orElseThrow();
@@ -142,7 +143,7 @@ public class ClientVendorServiceImpl implements ClientVendorService {
         clientVendorRepository.save(convertedClientVendor);
 
         ClientVendor saved = clientVendorRepository.save(convertedClientVendor);
-        return mapperUtil.convert(saved,new ClientVendorDto());
+        return mapperUtil.convert(saved, new ClientVendorDto());
     }
 
     @Override
@@ -158,17 +159,6 @@ public class ClientVendorServiceImpl implements ClientVendorService {
                 clientVendorDto.setHasInvoice(true);
             }
         }
+
     }
-
-    //    @Override
-//    public List<String> getCountries() {
-//        ResponseEntity<List<CountryDto>> countries = countryFeignClient.getApiCountries(API_COUNTRIES_KEY);
-//        if (countries.getStatusCode().is2xxSuccessful()){
-//            return countries.getBody().stream()
-//                    .map(CountryDto::getName)
-//                    .collect(Collectors.toList());
-//        }
-//        return List.of();
-//    }
-
 }
